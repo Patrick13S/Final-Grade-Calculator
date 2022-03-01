e992dfab5d9f8901312cb5daf3d4ab2196315848
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-03-01 14:15:27 -0700

    labels done

--- Final Grade Calculator/ViewController.swift
+++ Final Grade Calculator/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var toplabel: UILabel!
+    
+    @IBOutlet weak var currentgradelabel: UILabel!
+    
+    @IBOutlet weak var desiredfinalgradelabel: UILabel!
+    
+    @IBOutlet weak var percentageexamisworthlabel: UILabel!
+    
+    @IBOutlet weak var resultlabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currentgradelabel.text = "Current Grade"
+        toplabel.text = "Final Grade Calculator"
+        desiredfinalgradelabel.text = "Desired final grade"
+        percentageexamisworthlabel.text = "Percentage exam is worth"
     }
 
-
+    @IBAction func currentgradetextfield(_ sender: Any)
+    {
+    }
+    
+    @IBAction func desiredfinalgradetextfield(_ sender: Any)
+    {
+    }
+    
+    @IBAction func percentageexamisworthtextfield(_ sender: Any)
+    {
+    }
+    
+    
+    
+    
+   
+    @IBAction func calculatebutton(_ sender: Any)
+    {
+    }
+    
+    
 }
 
